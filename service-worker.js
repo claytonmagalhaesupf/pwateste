67c9836b86e5435c921d53f024e968936dce9c49
clayton <claytonmagalhaes@upf.br>
2025-03-31 00:11:16 -0300

    teste

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,25 @@
 const staticPwaADS = "cache_web_app_ads_upf"
 const assets = [
  "/",
- "/index.html",
-
+ "pwateste/index.html",
+ "pwateste/pwateste/formulario.html",
+ "pwateste/pwateste/outros.html",
+ "pwateste/fotos.html",
+ "pwateste/manifest.json",
+ "pwateste/panda.png",
+ "pwateste/sea_turtle.jpg",
+ "pwateste/elefante.jpg",
+ "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css",
+ "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js",
+ "pwateste/icons/icon-512x512.png",
+ "pwateste/icons/icon-384x384.png",
+ "pwateste/icons/icon-256x256.png",
+ "pwateste/icons/icon-192x192.png",
+ "pwateste/icons/icon-152x152.png",
+ "pwateste/icons/icon-144x144.png",
+ "pwateste/icons/icon-96x96.png",
+ "pwateste/icons/icon-72x72.png",
+ "pwateste/icons/icon-48x48.png",
 ]
 
 self.addEventListener("install", installEvent => {
